e12468c3ed36c8ff9b301278b98978dd4bb4f901
kdg <kdg@kdgs-MacBook-Air.local>
2023-06-27 23:09:24 +0530

    small change

--- src/main/java/org/com/commands/BalanceCommand.java
+++ src/main/java/org/com/commands/BalanceCommand.java
@@ -74,58 +74,63 @@ public class BalanceCommand {
     public void rebalance(String line, IntitialInvestments intitalInvestments, SipPayments sipPayments, Map<String, MonthlyChange> monthChangeMap) {
 
         int monthIndex = Calendar.getInstance().get(Calendar.MONTH) + 1;
-        int equityBalance = 0;
-        int debtBalance = 0;
-        int goldBalance = 0;
+        if (monthIndex == 6 || monthIndex == 12) {
+            int equityBalance = 0;
+            int debtBalance = 0;
+            int goldBalance = 0;
 
 
-        for (int i = 1; i <= monthIndex; i++) {
-            String tempMonth = null;
-            if (HelperUtil.reverseMonths().containsKey(i)) {
-                tempMonth = HelperUtil.reverseMonths().get(i);
-            } else {
-                System.out.println("CANNOT_REBALANCE");
-                return;
+            for (int i = 1; i <= monthIndex; i++) {
+                String tempMonth = null;
+                if (HelperUtil.reverseMonths().containsKey(i)) {
+                    tempMonth = HelperUtil.reverseMonths().get(i);
+                } else {
+                    System.out.println("CANNOT_REBALANCE");
+                    return;
+                }
+
+                MonthlyChange mp = null;
+                if (monthChangeMap.containsKey(tempMonth)) {
+                    mp = monthChangeMap.get(tempMonth);
+                } else {
+                    System.out.println("CANNOT_REBALANCE");
+                    return;
+                }
+
+
+                if (i == 1) {
+                    equityBalance = (int) (equityBalance + intitalInvestments.getEquity() + (intitalInvestments.getEquity() * mp.getEquityChange()) / 100);
+                    debtBalance = (int) (debtBalance + intitalInvestments.getDebt() + (intitalInvestments.getDebt() * mp.getDebtChange()) / 100);
+                    goldBalance = (int) (goldBalance + intitalInvestments.getGold() + (intitalInvestments.getGold() * mp.getGoldChange()) / 100);
+                } else {
+                    int tempEquityVal = equityBalance + sipPayments.getEquity();
+                    int tempDebtVal = debtBalance + sipPayments.getDebt();
+                    int tempGoldValue = goldBalance + sipPayments.getGold();
+                    equityBalance = (int) (tempEquityVal + ((tempEquityVal * mp.getEquityChange()) / 100));
+                    debtBalance = (int) (tempDebtVal + ((tempDebtVal * mp.getDebtChange()) / 100));
+                    goldBalance = (int) (tempGoldValue + ((tempGoldValue * mp.getGoldChange()) / 100));
+                }
+
             }
 
-            MonthlyChange mp = null;
-            if (monthChangeMap.containsKey(tempMonth)) {
-                mp = monthChangeMap.get(tempMonth);
-            } else {
-                System.out.println("CANNOT_REBALANCE");
-                return;
-            }
+            int currentTotal = equityBalance + debtBalance + goldBalance;
+            int initialTotal = intitalInvestments.getEquity() + intitalInvestments.getGold() + intitalInvestments.getDebt();
+            int balancedEquityAllocation = (int) ((intitalInvestments.getEquity() * currentTotal) / initialTotal);
+            int balancedDebtAllocation = (int) ((intitalInvestments.getDebt() * currentTotal) / initialTotal);
+            int balancedGoldAllocation = (int) ((intitalInvestments.getGold() * currentTotal) / initialTotal);
 
+            System.out.println();
+            System.out.print(balancedEquityAllocation);
+            System.out.print("\t");
+            System.out.print(balancedDebtAllocation);
+            System.out.print("\t");
+            System.out.print(balancedGoldAllocation);
+            System.out.print("\t");
 
-            if (i == 1) {
-                equityBalance = (int) (equityBalance + intitalInvestments.getEquity() + (intitalInvestments.getEquity() * mp.getEquityChange()) / 100);
-                debtBalance = (int) (debtBalance + intitalInvestments.getDebt() + (intitalInvestments.getDebt() * mp.getDebtChange()) / 100);
-                goldBalance = (int) (goldBalance + intitalInvestments.getGold() + (intitalInvestments.getGold() * mp.getGoldChange()) / 100);
-            } else {
-                int tempEquityVal = equityBalance + sipPayments.getEquity();
-                int tempDebtVal = debtBalance + sipPayments.getDebt();
-                int tempGoldValue = goldBalance + sipPayments.getGold();
-                equityBalance = (int) (tempEquityVal + ((tempEquityVal * mp.getEquityChange()) / 100));
-                debtBalance = (int) (tempDebtVal + ((tempDebtVal * mp.getDebtChange()) / 100));
-                goldBalance = (int) (tempGoldValue + ((tempGoldValue * mp.getGoldChange()) / 100));
-            }
-
+        }else{
+            System.out.println("CAN BE COMPUTED ONLY FOR JUNE and DEC MONTH");
+            return;
         }
-
-        int currentTotal = equityBalance + debtBalance + goldBalance;
-        int initialTotal = intitalInvestments.getEquity() + intitalInvestments.getGold() + intitalInvestments.getDebt();
-        int balancedEquityAllocation = (int) ((intitalInvestments.getEquity() * currentTotal) / initialTotal);
-        int balancedDebtAllocation = (int) ((intitalInvestments.getDebt() * currentTotal) / initialTotal);
-        int balancedGoldAllocation = (int) ((intitalInvestments.getGold() * currentTotal) / initialTotal);
-
-        System.out.println();
-        System.out.print(balancedEquityAllocation);
-        System.out.print("\t");
-        System.out.print(balancedDebtAllocation);
-        System.out.print("\t");
-        System.out.print(balancedGoldAllocation);
-        System.out.print("\t");
-
     }
 
 }
